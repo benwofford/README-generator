555604a975fe1202a169876c91f7b5c477117c88
BenWofford <woffordbt@gmail.com>
2021-10-04 21:56:08 -0400

    added packages and question array

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Provide a short description explaining the purpose of your project.",
+      name: "description",
+    },
+    {
+        type: "input",
+        message: "List the appropriate sections to build the Table of Contents.",
+        name: "table-of-contents",
+      },
+    {
+      type: "input",
+      message: "Describe the steps required to install your project.",
+      name: "installation",
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "usage",
+      },
+    {
+        type: "list",
+        message: "Please choose an applicable license for your project.",
+        name: "license",
+      },
+      {
+        type: "input",
+        message: "How can others contribute to this project?",
+        name: "contributing",
+      },
+      {
+        type: "input",
+        message: "Describe the tests have you run for your application.",
+        name: "tests",
+      },
+      {
+        type: "input",
+        message: "Please enter any questions.",
+        name: "questions",
+      },
+  ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
