a52e595a5317405aee6ba207217c5ded86839b90
BenWofford <woffordbt@gmail.com>
2021-10-05 23:07:30 -0400

    pushing changes worked out with tutor and final build

--- index.js
+++ index.js
@@ -1,63 +1,77 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const fs = require("fs");
+const fs = require("fs").promises;
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-// const questions = [];
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is the title of your project?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Provide a short description explaining the purpose of your project.",
-      name: "description",
-    },
-    {
+ const questions = [
+      {
         type: "input",
-        message: "List the appropriate sections to build the Table of Contents.",
-        name: "table-of-contents",
-      },
-    {
-      type: "input",
-      message: "Describe the steps required to install your project.",
-      name: "installation",
-    },
-    {
-        type: "input",
-        message: "Provide instructions and examples for use.",
-        name: "usage",
-      },
-    {
-        type: "list",
-        message: "Please choose an applicable license for your project.",
-        name: "license",
+        message: "What is the title of your project?",
+        name: "title",
       },
       {
         type: "input",
-        message: "How can others contribute to this project?",
+        message:
+          "Provide a short description explaining the purpose of your project.",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "List the appropriate sections to build the Table of Contents.",
+        name: "table",
+      },
+      {
+        type: "input",
+        message: "Describe the steps required to install your project.",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "What are some best practices for usage?",
+        name: "usage",
+      },
+      {
+        type: "list",
+        message: "Please choose an applicable license for your project.",
+        name: "license",
+        choices: ["MIT", "Apache", "none"]
+      },
+      {
+        type: "input",
+        message: "May others contribute to this project? If so, how?",
         name: "contributing",
       },
       {
         type: "input",
-        message: "Describe the tests have you run for your application.",
+        message: "Describe the tests you have run for your application.",
         name: "tests",
       },
       {
         type: "input",
-        message: "Please enter any questions.",
+        message: "Please enter your github username and email.",
         name: "questions",
       },
-  ])
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    ];
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+  
+    // TODO: Create a function to write README file
+    // function writeToFile(fileName, data) {}
+    .then(
+      (answers) => {
+        return fs.writeFile(
+          `README.md`,
+          generateMarkdown(answers),      
+        );
+      }
+    )
+    .then(() => console.log("Enjoy your README!"))
+    .catch((err) => console.log(err));
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+} else if (license === 'Apache') {
+  return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+} else (license === 'none')
+  return " ";
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+      const mit = "MIT";
+      const mitLink = mit.link("https://www.mit.edu/~amini/LICENSE.md");
+      return mit;
+  } else if (license === 'Apache') {
+      const apache = "Apache";
+      const apacheLink = apache.link("https://www.apache.org/licenses/LICENSE-2.0");
+      return apache;
+  } else (err) => console.log(error);
+};
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown({
+  title,
+  description,
+  table,
+  installation,
+  usage,
+  license,
+  contributing,
+  tests,
+  questions,
+}) {
+  return
+  `# ${title} #
+${renderLicenseBadge(license)};
 
-`;
-}
+## Description ##
+${description}
+
+## Table of Contents ##
+${table}
+
+## Installation ##
+${installation}
+
+## Usage ##
+${usage}
+
+## License ##
+${renderLicenseLink}
+
+## Contribute ##
+${contributing}
+
+## Tests ##
+${tests}
+
+## Questions ##
+Any questions? Find me on Github! Shoot me an email!
+${questions}
+`
+};
 
 module.exports = generateMarkdown;
